9cb149db9756e6c13b58c10ce4d9da507f3abb15
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-05 16:44:41 +0000

    Check for losses

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 import chess
+from termcolor import colored
+import sys
+
+is_legal = True
+board = chess.Board()
+
+def loss():
+    global board
+
+    if board.turn == True:
+        print(colored("White Lost", "red"))
+    else:
+        print(colored("Black Lost", "red"))
