64bbb21afe6e45b3ad54f78369031f89381b9639
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-01-05 16:48:53 +0000

    Creating the main function

--- src/main.py
+++ src/main.py
@@ -2,13 +2,31 @@ import chess
 from termcolor import colored
 import sys
 
-is_legal = True
+isLegal = True
 board = chess.Board()
 
 def loss():
     global board
-
     if board.turn == True:
         print(colored("White Lost", "red"))
     else:
         print(colored("Black Lost", "red"))
+
+    newGame = input("Play Again? (y/n): ")
+    if newGame == "y":
+        board.clearStack()
+        chessGame()
+    elif newGame == "n":
+        sys.exit()
+
+def chessGame():
+    global board
+
+    def move(board):
+        if board.turn == True:
+            print(colored(str(board), attrs=["bold"]))
+            playerMove = input("White to Move: ")
+        elif board.turn == False:
+            print(colored(str(board)[::-1], attrs=["bold"]))
+            playerMove = input("Black to Move: ")
+        if 
